a0ada7495457a0d61d35967b490301455ab47f74
pthanomsing <p.thanomsing@outlook.com>
2022-07-11 10:04:17 +0700

    Complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+const repeatString = function(message,times) {
+
+    let messageRepeat = '';
+
+    if (times >= 0) {
+        for (i = 0; i < times; i++) {
+            messageRepeat = messageRepeat + message;
+        }
+
+        return messageRepeat;
+    }
+    else {
+        return 'ERROR'
+    }
 
 };
 
