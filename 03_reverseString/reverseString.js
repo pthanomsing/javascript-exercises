524109302e3b458ae4e3f11adec7ea8522fe8b28
pthanomsing <p.thanomsing@outlook.com>
2022-07-11 10:23:12 +0700

    Complete exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(message) {
+    
+    message = message.toString();
+    
+    let length = message.length;
 
+    let finalMessage = '';
+
+    for(i = 0; i < length; i++) {
+        finalMessage += message.slice(message.length - 1);
+        message = message.slice(0,-1);
+       
+    }
+
+    return finalMessage;
 };
 
 // Do not edit below this line
